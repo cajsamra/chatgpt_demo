a483ceb7d40f84b37b5399e1315ae599001ae5e4
cajsamra <30735556+cajsamra@users.noreply.github.com>
2023-07-23 16:08:51 -0700

    Add files via upload
    Separated config stuff so it is clearer for the user. Plug your API KEY in here.

---
+++ OpenAIConfig.js
@@ -0,0 +1,4 @@
+  const API_URL = "https://api.openai.com/v1/chat/completions";
+  const API_KEY = '<your key>';
+  
+  const ROLE_DESC = "You are a biographer and book editor. Read the content presented to you and create a summary."
